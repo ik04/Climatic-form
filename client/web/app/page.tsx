5fa75c75b065ed9aab16788dcad808273ee6efbc
ishaan khurana <ishaankhurana123@gmail.com>
2024-06-16 20:30:51 +0530

    chore: finish forms.json till page 3

--- client/web/app/page.tsx
+++ client/web/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>hi mom</div>;
+  return <div></div>;
 };
 
 export default page;
--- client/web/tailwind.config.ts
+++ client/web/tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans"],
+      jost: ["Jost", "sans"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
